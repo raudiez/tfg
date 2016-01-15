b37fcdea67fd895f9fab5fd444fabbb17f478085
Raúl Díez Sánchez <rauldiez20@gmail.com>
2016-01-15 14:00:46 +0100

    Cleaning.

--- src/android/DeafAlert/app/src/main/java/es/uca/tfg_rau/deafalert/AlertListAdapter.java
+++ src/android/DeafAlert/app/src/main/java/es/uca/tfg_rau/deafalert/AlertListAdapter.java
@@ -1,7 +1,6 @@
 package es.uca.tfg_rau.deafalert;
 
 import android.app.Activity;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,13 +13,11 @@ import java.util.ArrayList;
  * Created by tfg_rau on 2/01/16.
  */
 public class AlertListAdapter extends BaseAdapter{
-    private Activity activity;
     private ArrayList<String> alerts;
     private static LayoutInflater inflater = null;
 
 
     public AlertListAdapter(Activity activity, ArrayList<String> alerts){
-        this.activity = activity;
         this.alerts = alerts;
         this.inflater = activity.getLayoutInflater();
     }
